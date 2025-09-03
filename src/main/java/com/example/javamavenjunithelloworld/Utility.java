738c222b9a1d510c6cb0a257d6cd33f3e843fdb8
saiganesh9000 <155514093+saiganesh9000@users.noreply.github.com>
2025-09-03 12:24:17 +0530

    VARANASIS0001 - Added Utility.java with functions

--- src/main/java/com/example/javamavenjunithelloworld/Utility.java
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -2,16 +2,14 @@ package com.example.javamavenjunithelloworld;
 
 public class Utility {
 
-    public static void checkOddEven(int num) {
-        if (num % 2 == 0) {
-            System.out.println(num + " is Even");
-        } else {
-            System.out.println(num + " is Odd");
-        }
+    public static double areaOfCircle(double radius) {
+        return Math.PI * radius * radius;
+    }
+    public static int addNumbers(int a, int b) {
+        return a + b;
     }
 
-    public static void varanasi() {
+    public static void printMyName() {
         System.out.println("I am Varanasi Sai Ganesh");
     }
 }
- 
