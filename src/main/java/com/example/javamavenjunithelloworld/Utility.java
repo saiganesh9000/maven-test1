5eae554c4127a4ecedae3aeb524fdc765741c895
saiganesh9000 <155514093+saiganesh9000@users.noreply.github.com>
2025-09-03 12:19:46 +0530

    VARANASIS0001 - Created Utility.java with odd/even and name function

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,16 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+
+    public static void checkOddEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+
+    public static void varanasi() {
+        System.out.println("I am Varanasi Sai Ganesh");
+    }
+}
